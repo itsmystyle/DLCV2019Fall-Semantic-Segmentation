7ebd13793542fcaea87925686cf2752347a13ad4
YwuanChai <itsmystyle.0821@gmail.com>
2019-10-26 17:08:28 +0800

    Format Document

--- mean_iou_evaluate.py
+++ mean_iou_evaluate.py
@@ -1,26 +1,29 @@
-import numpy as np
-import scipy.misc
-import argparse
 import os
 import glob
 
+import argparse
+import scipy.misc
+import numpy as np
+
+
 def read_masks(seg_dir, seg_h=352, seg_w=448):
-    '''
+    """
     Read masks from directory
-    '''
-    seg_paths = glob.glob(os.path.join(seg_dir,'*.png'))
+    """
+    seg_paths = glob.glob(os.path.join(seg_dir, "*.png"))
     seg_paths.sort()
     segs = np.zeros((len(seg_paths), seg_h, seg_w))
     for idx, seg_path in enumerate(seg_paths):
         seg = scipy.misc.imread(seg_path)
         segs[idx, :, :] = seg
-    
+
     return segs
 
+
 def mean_iou_score(pred, labels, num_classes=9):
-    '''
+    """
     Compute mean IoU score over 9 classes
-    '''
+    """
     mean_iou = 0
     for i in range(num_classes):
         tp_fp = np.sum(pred == i)
@@ -28,21 +31,21 @@ def mean_iou_score(pred, labels, num_classes=9):
         tp = np.sum((pred == i) * (labels == i))
         iou = tp / (tp_fp + tp_fn - tp)
         mean_iou += iou / num_classes
-        print('class #%d : %1.5f'%(i, iou))
-    print('\nmean_iou: %f\n' % mean_iou)
+        print("class #%d : %1.5f" % (i, iou))
+    print("\nmean_iou: %f\n" % mean_iou)
 
     return mean_iou
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-g', '--labels', help='ground truth masks directory', type=str)
-    parser.add_argument('-p', '--pred', help='prediction masks directory', type=str)
+    parser.add_argument("-g", "--labels", help="ground truth masks directory", type=str)
+    parser.add_argument("-p", "--pred", help="prediction masks directory", type=str)
     args = parser.parse_args()
 
     pred = read_masks(args.pred)
     labels = read_masks(args.labels)
-        
-    print('# preds: {}; pred.shape: {}'.format(len(pred), pred.shape))
-    print('# labels: {}; labels.shape: {}'.format(len(labels), labels.shape))
+
+    print("# preds: {}; pred.shape: {}".format(len(pred), pred.shape))
+    print("# labels: {}; labels.shape: {}".format(len(labels), labels.shape))
     mean_iou_score(pred, labels)
