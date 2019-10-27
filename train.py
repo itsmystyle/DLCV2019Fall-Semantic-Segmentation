bc7fc063d674fbb5b7937194769affbe37f5527c
YwuanChai <itsmystyle.0821@gmail.com>
2019-10-27 17:37:39 +0800

    renamed trainer to train, created trainer class

---
+++ train.py
@@ -0,0 +1,76 @@
+import os
+
+import torch
+import numpy as np
+import torch.nn as nn
+from tensorboardX import SummaryWriter
+
+import data
+import parser
+from metrics import MeanIOUScore
+from modules.trainer import Trainer
+from modules.baseline_model import BaselineNet
+
+
+if __name__ == "__main__":
+
+    args = parser.arg_parse()
+
+    """create directory to save trained model and other info"""
+    if not os.path.exists(args.save_dir):
+        os.makedirs(args.save_dir)
+
+    """ setup GPU """
+    torch.cuda.set_device(args.gpu)
+
+    """ setup random seed """
+    np.random.seed(args.random_seed)
+    torch.manual_seed(args.random_seed)
+    torch.cuda.manual_seed(args.random_seed)
+
+    """ load dataset and prepare data loader """
+    print("===> prepare dataloader ...")
+    train_loader = torch.utils.data.DataLoader(
+        data.SegData(os.path.join(args.data_dir, "train"), "train"),
+        batch_size=args.train_batch,
+        num_workers=args.workers,
+        shuffle=True,
+    )
+    val_loader = torch.utils.data.DataLoader(
+        data.SegData(os.path.join(args.data_dir, "val"), "valid"),
+        batch_size=args.train_batch,
+        num_workers=args.workers,
+        shuffle=False,
+    )
+
+    """ load model """
+    print("===> prepare model ...")
+    model = BaselineNet(args)
+    model.cuda()
+
+    """ define loss """
+    criterion = nn.CrossEntropyLoss()
+
+    """ setup metrics """
+    metric = MeanIOUScore(9)
+
+    """ setup optimizer """
+    optimizer = torch.optim.Adam(
+        model.parameters(), lr=args.lr, weight_decay=args.weight_decay
+    )
+
+    """ setup tensorboard """
+    writer = SummaryWriter(os.path.join(args.save_dir, "train_info"))
+
+    trainer = Trainer(
+        model,
+        optimizer,
+        criterion,
+        train_loader,
+        val_loader,
+        writer,
+        metric,
+        args.save_dir,
+    )
+
+    trainer.fit(args.epochs)
