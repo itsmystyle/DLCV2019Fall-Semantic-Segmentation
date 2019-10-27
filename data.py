826e0388d8c88a144f2c7ecec33e0bb99f043576
YwuanChai <itsmystyle.0821@gmail.com>
2019-10-27 11:51:02 +0800

    fixed minor bugs

--- data.py
+++ data.py
@@ -3,14 +3,14 @@ import glob
 
 import torch
 import argparse
-import numpy as np
+import scipy.misc
 import torchvision.transforms as transforms
 from PIL import Image
 from torch.utils.data import Dataset
 
 
-MEAN = [0.5, 0.5, 0.5]
-STD = [0.5, 0.5, 0.5]
+MEAN = [0.485, 0.456, 0.406]
+STD = [0.229, 0.224, 0.225]
 
 
 class SegData(Dataset):
@@ -23,8 +23,10 @@ class SegData(Dataset):
         self.seg_dir = os.path.join(self.data_dir, "seg")
 
         """ read the data list """
-        imgs_path = sorted(glob.glob(os.path.join(self.img_dir, "*.png")))
-        segs_path = sorted(glob.glob(os.path.join(self.seg_dir, "*.png")))
+        imgs_path = glob.glob(os.path.join(self.img_dir, "*.png"))
+        imgs_path.sort()
+        segs_path = glob.glob(os.path.join(self.seg_dir, "*.png"))
+        segs_path.sort()
 
         """ set up image path """
         if self.mode == "test":
@@ -34,10 +36,7 @@ class SegData(Dataset):
 
         """ set up image trainsform """
         self.transform = transforms.Compose(
-            [
-                transforms.ToTensor(),  # (H,W,C)->(C,H,W), [0,255]->[0, 1.0] RGB->RGB
-                transforms.Normalize(MEAN, STD),
-            ]
+            [transforms.ToTensor(), transforms.Normalize(MEAN, STD)]
         )
 
     def __len__(self):
@@ -59,12 +58,9 @@ class SegData(Dataset):
 
             """ read image/seg and convert to tensor"""
             img = Image.open(img_path).convert("RGB")
-            seg = Image.open(seg_path)
+            seg = scipy.misc.imread(seg_path)
 
-            return (
-                self.transform(img),
-                torch.from_numpy(np.array(seg, np.int32, copy=False)).long(),
-            )
+            return (self.transform(img), torch.from_numpy(seg).long())
 
 
 if __name__ == "__main__":
@@ -75,12 +71,18 @@ if __name__ == "__main__":
         default=os.path.join("hw2_data", "train"),
         help="root path to data directory",
     )
+    parser.add_argument(
+        "--mode",
+        type=str,
+        default="train",
+        help="Dataloader mode for train/valid/test.",
+    )
     args = parser.parse_args()
 
-    seg_data = SegData(args.data_dir)
+    seg_data = SegData(args.data_dir, args.mode)
 
     from torch.utils.data import DataLoader
 
-    dataloader = DataLoader(seg_data, batch_size=32, shuffle=False, num_workers=4)
+    dataloader = DataLoader(seg_data, batch_size=32, shuffle=False, num_workers=0)
     for batch in dataloader:
         break
