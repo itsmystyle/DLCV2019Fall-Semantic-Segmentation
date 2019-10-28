b33a065fef590dd2a86db215769506b4d16880bb
YwuanChai <itsmystyle.0821@gmail.com>
2019-10-28 12:08:47 +0800

    changed test mode to return image and filename

--- data.py
+++ data.py
@@ -7,6 +7,7 @@ import scipy.misc
 import torchvision.transforms as transforms
 from PIL import Image
 from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
 
 
 MEAN = [0.485, 0.456, 0.406]
@@ -15,23 +16,28 @@ STD = [0.229, 0.224, 0.225]
 
 class SegData(Dataset):
     def __init__(self, data_dir, mode="test"):
-
-        """ set up basic parameters for dataset """
         self.mode = mode
         self.data_dir = data_dir
-        self.img_dir = os.path.join(self.data_dir, "img")
-        self.seg_dir = os.path.join(self.data_dir, "seg")
 
-        """ read the data list """
-        imgs_path = glob.glob(os.path.join(self.img_dir, "*.png"))
-        imgs_path.sort()
-        segs_path = glob.glob(os.path.join(self.seg_dir, "*.png"))
-        segs_path.sort()
-
-        """ set up image path """
+        """ set up data """
         if self.mode == "test":
+            """ read the data list """
+            imgs_path = glob.glob(self.data_dir, "*.png")
+            imgs_path.sort()
+
             self.data = imgs_path
+
         else:
+            """ set up basic parameters for train/validation dataset """
+            self.img_dir = os.path.join(self.data_dir, "img")
+            self.seg_dir = os.path.join(self.data_dir, "seg")
+
+            """ read the data list """
+            imgs_path = glob.glob(os.path.join(self.img_dir, "*.png"))
+            imgs_path.sort()
+            segs_path = glob.glob(os.path.join(self.seg_dir, "*.png"))
+            segs_path.sort()
+
             self.data = [p for p in zip(imgs_path, segs_path)]
 
         """ set up image trainsform """
@@ -51,7 +57,8 @@ class SegData(Dataset):
             """ read image and convert to tensor"""
             img = Image.open(img_path).convert("RGB")
 
-            return self.transform(img)
+            """ TODO: Split path into filename only """
+            return self.transform(img), img_path
         else:
             """ get data """
             img_path, seg_path = self.data[idx]
@@ -72,17 +79,16 @@ if __name__ == "__main__":
         help="root path to data directory",
     )
     parser.add_argument(
-        "--mode",
-        type=str,
-        default="train",
-        help="Dataloader mode for train/valid/test.",
+        "--mode", type=str, default="train", help="Dataloader mode for train/valid/test."
     )
     args = parser.parse_args()
 
     seg_data = SegData(args.data_dir, args.mode)
 
-    from torch.utils.data import DataLoader
-
     dataloader = DataLoader(seg_data, batch_size=32, shuffle=False, num_workers=0)
     for batch in dataloader:
+        """ TODO: Unit-test on test mode """
+        import ipdb
+
+        ipdb.set_trace()
         break
