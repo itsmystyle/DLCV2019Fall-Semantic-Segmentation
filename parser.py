b77fe7adc40849a6672e39de556bf44c20f6fffb
YwuanChai <itsmystyle.0821@gmail.com>
2019-10-28 12:18:35 +0800

    added output directory argument

--- parser.py
+++ parser.py
@@ -5,38 +5,32 @@ import argparse
 
 
 def arg_parse():
-    parser = argparse.ArgumentParser(
-        description="DLCV HW2 in image segmentation using pytorch"
-    )
+    parser = argparse.ArgumentParser(description="DLCV HW2 in image segmentation using pytorch")
 
     # Datasets parameters
     parser.add_argument(
-        "--data_dir",
-        type=str,
-        default=os.path.join("hw2_data"),
-        help="root path to data directory",
+        "--data_dir", type=str, default=os.path.join("hw2_data"), help="root path to data directory"
     )
     parser.add_argument(
-        "--workers",
-        default=4,
-        type=int,
-        help="number of data loading workers (default: 4)",
+        "--workers", default=4, type=int, help="number of data loading workers (default: 4)"
+    )
+    parser.add_argument(
+        "--output_dir",
+        type=str,
+        default=os.path.join("hw2_data"),
+        help="root path to output directory",
     )
 
     # Models parameters
     parser.add_argument("--save_dir", type=str, default="models")
     parser.add_argument("--pretrained", dest="pretrained", action="store_true")
-    parser.add_argument(
-        "--resume", type=str, default="", help="path to the trained model"
-    )
+    parser.add_argument("--resume", type=str, default="", help="path to the trained model")
 
     # Training parameters
     parser.add_argument(
         "--gpu", default=0, type=int, help="gpu device ids for CUDA_VISIBLE_DEVICES"
     )
-    parser.add_argument(
-        "--epochs", default=100, type=int, help="num of validation iterations"
-    )
+    parser.add_argument("--epochs", default=100, type=int, help="num of validation iterations")
     parser.add_argument("--train_batch", default=32, type=int, help="train batch size")
     parser.add_argument("--test_batch", default=32, type=int, help="test batch size")
     parser.add_argument("--lr", default=2e-4, type=float, help="initial learning rate")
