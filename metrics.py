43220e3b59fb2114c3db34759f3961bfaa01b305
YwuanChai <itsmystyle.0821@gmail.com>
2019-10-31 22:09:27 +0800

    changed queue to store mean iou

--- metrics.py
+++ metrics.py
@@ -16,31 +16,35 @@ class Metrics:
 
 
 class MeanIOUScore(Metrics):
-    def __init__(self, num_classes, window_size=100):
-        self.name = "Mean IOU"
+    def __init__(self, num_classes, window_size=128):
+        self.name = "mIoU"
         self.num_classes = num_classes
         self.window_size = window_size
-        self.scores = []
+        self.tp_fp = [0.0] * self.num_classes
+        self.tp_fn = [0.0] * self.num_classes
+        self.tp = [0.0] * self.num_classes
+        self.score = 0.0
 
     def reset(self):
-        self.scores = []
+        self.tp_fp = [0.0] * self.num_classes
+        self.tp_fn = [0.0] * self.num_classes
+        self.tp = [0.0] * self.num_classes
+        self.score = 0.0
 
     def update(self, predicts, targets):
         mean_iou = 0.0
         for i in range(self.num_classes):
             _preds = predicts == i
             _tars = targets == i
-            tp_fp = _preds.sum()
-            tp_fn = _tars.sum()
-            tp = (_preds * _tars).sum()
-            iou = tp / (tp_fp + tp_fn - tp + 1e-20)
+            self.tp_fp[i] += _preds.sum()
+            self.tp_fn[i] += _tars.sum()
+            self.tp[i] += (_preds * _tars).sum()
+            iou = self.tp[i] / (self.tp_fp[i] + self.tp_fn[i] - self.tp[i] + 1e-20)
             mean_iou += iou / self.num_classes
-        self.scores.append(mean_iou)
-        ws = self.window_size
-        self.scores = self.scores[-ws:]
+        self.score = mean_iou
 
     def get_score(self):
-        return np.mean(self.scores)
+        return self.score
 
     def print_score(self):
         score = self.get_score()
