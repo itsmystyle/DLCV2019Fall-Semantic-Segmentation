66fc33fc33ab3dbdcaf1067a1e23715973e93146
YwuanChai <itsmystyle.0821@gmail.com>
2019-11-03 16:57:21 +0800

    changed improved model back

--- modules/utils.py
+++ modules/utils.py
@@ -1,6 +1,5 @@
 from modules.baseline_model import BaselineNet
 from modules.improved_unet_model import ImprovedUNet
-from modules.experiment_unet_model import r0ImprovedUNet
 
 
 def prepare_model(args):
@@ -9,6 +8,6 @@ def prepare_model(args):
         model = BaselineNet(args)
     else:
         print("===> prepare IMPROVEMENT model ...")
-        model = r0ImprovedUNet(args)
+        model = ImprovedUNet(args)
 
     return model
