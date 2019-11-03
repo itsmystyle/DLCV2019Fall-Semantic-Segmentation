91ce74cd0c18939f2be77c8c04c286f395ed284b
YwuanChai <itsmystyle.0821@gmail.com>
2019-11-03 19:35:39 +0800

    added shell command

--- hw2.sh
+++ hw2.sh
@@ -1,4 +1,5 @@
 # TODO: create shell script for running the testing code of the baseline model
 
-# wget -O models/baseline_model/model_best.pth.tar
-python3 test.py --resume models/baseline_v2/model_best.pth.tar --test_batch 24 --data_dir $1 --output_dir $2 --baseline
+mkdir -p models/baseline_model/
+wget 'https://www.dropbox.com/s/wd7xcrumcnzv2d6/improved_model_best.pth.tar?dl=1' -O models/baseline_model/model_best.pth.tar
+python3 test.py --resume models/baseline_model/model_best.pth.tar --test_batch 8 --data_dir $1 --output_dir $2 --baseline
