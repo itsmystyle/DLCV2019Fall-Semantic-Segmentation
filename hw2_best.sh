25bdef1f649b5c5c7f29e016256c2e922f0e5797
YwuanChai <itsmystyle.0821@gmail.com>
2019-11-04 16:43:02 +0800

    added models folder and removed mkdir

--- hw2_best.sh
+++ hw2_best.sh
@@ -1,5 +1,4 @@
 # TODO: create shell script for running the testing code of your improved model
 
-mkdir -p models/improved_model/
 wget 'https://www.dropbox.com/s/wd7xcrumcnzv2d6/improved_model_best.pth.tar?dl=1' -O models/improved_model/model_best.pth.tar
 python3 test.py --resume models/improved_model/model_best.pth.tar --test_batch 8 --data_dir $1 --output_dir $2
